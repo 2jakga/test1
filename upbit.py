9ed9c5aeefdb243a785086d93124a53c7c54b135
2jakga <90486371+2jakga@users.noreply.github.com>
2021-09-12 01:51:15 +0900

    Update upbit.py

--- upbit.py
+++ upbit.py
@@ -7,6 +7,9 @@ import schedule
 access = "m0uV5VEpMw9uNrvDlxUXvahxvYYG2O3KGNW3vRsJ"
 secret = "iRmSf6ovpVTpSzPnyxDLYEMcvKYmbsPo9QE8gQuw"
 
+itemname = "KRW-OMG"
+Kvalue = 0.2
+
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
@@ -35,99 +38,65 @@ def get_current_price(ticker):
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
 
-
-# predicted_close_price = 0
-# def predict_price(ticker):
-#     """Prophet으로 당일 종가 가격 예측"""
-#     global predicted_close_price
-#     df = pyupbit.get_ohlcv(ticker, interval="minute60")
-#     df = df.reset_index()
-#     df['ds'] = df['index']
-#     df['y'] = df['close']
-#     data = df[['ds','y']]
-#     model = Prophet()
-#     model.fit(data)
-#     future = model.make_future_dataframe(periods=24, freq='H')
-#     forecast = model.predict(future)
-#     closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=17)]
-#     if len(closeDf) == 0:
-#         closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=17)]
-#     closeValue = closeDf['yhat'].values[0]
-#     predicted_close_price = closeValue
-# predict_price("KRW-OMG")
-# schedule.every().hour.do(lambda: predict_price("KRW-OMG"))
-
-# 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start - V2 210911 ")
+print("autotrade start - V3 210911 ")
+a=1
+b=1
+c=1
 
 
-# 자동매매 시작
+
 while True:
     try:
-        now = datetime.datetime.now()
-        # start_time = get_start_time("KRW-OMG") - datetime.timedelta(hours=4)  #05:00
-        start_time = get_start_time("KRW-OMG")   #0:00
-        end_time = start_time + datetime.timedelta(hours=9)  #18:00 
-        schedule.run_pending()
-        target_price = get_target_price("KRW-OMG", 0.2)
-        current_price = get_current_price("KRW-OMG")
-        a=1
-        b=1
-        c=1
-        d=1
-
-        print("현재시간",now,"수익률:",(1-upbit.get_avg_buy_price('KRW-OMG')/current_price)*100,"%")
         
-            # 05:00~16:59 에 예측가격과 변동성돌파 조건에 만족하면 70% 매수 한다.
-        if start_time < now < end_time - datetime.timedelta(seconds=60): #09:00~17:59
-            # print("주간 매수")                            
-            
-            if target_price < current_price : #17시 예상가격보다 낮으면 매수
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-OMG", krw*0.7*0.9995) #보유금의 70% 매수 (수수로 감안)
-                    print(a,"매수 평균단가:",upbit.get_avg_buy_price('KRW-OMG')) 
-                    a+1
-            
-            if upbit.get_avg_buy_price('KRW-OMG')*1.05 < current_price :
-                btc = get_balance("BTC-OMG")
-
-                if btc > 0.00008:
-                     upbit.sell_market_order("KRW-OMG", btc)
-                     print("익절 횟수:",b) 
-                     b+1
-                   
-
-        else: # 18:00 ~ 08:59 까지 
-            # print("야간 매수") 
-          
-            
-            if c<2 and upbit.get_avg_buy_price('KRW-OMG') > 100 and current_price < upbit.get_avg_buy_price('KRW-OMG') * 0.95 : # 18시까지 수익 못본상태에서 -5%되면 추매(1/3) 
-                krw = get_balance("KRW")
-                if krw > 15000:
-                    upbit.buy_market_order("KRW-OMG", (krw/3)*0.9995) 
-                    print("1차 추가 매수")
-                    c+1
-                    print(c)
-
-           
-            if d<2 and upbit.get_avg_buy_price('KRW-OMG') > 100 and current_price < upbit.get_avg_buy_price('KRW-OMG') * 0.90 : # 18시까지 수익 못본상태에서 -10%되면 추매(All) 
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-OMG", krw*0.9995) 
-                    print("2차 추가 매수")       
-                    d+1
-                    print(d)
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1)
+        schedule.run_pending()
+        target_price = get_target_price("KRW-OMG", 0.3)
+        current_price = get_current_price("KRW-OMG")
+        krw = get_balance("KRW")
+        
+        print(target_price, current_price)        
+        print("현재시간",now,"수익률:",(1-upbit.get_avg_buy_price('KRW-OMG')/current_price)*100,"%")
                     
+        if start_time < now < end_time - datetime.timedelta(seconds=60): #09:00~17:59
+            if target_price < current_price : #예상가격이 현재가격보다 낮으면 매수 (상승신호 간주)
+                if krw > 5000:                 
+                     if a<2:                
+                        upbit.buy_market_order("KRW-OMG", krw*0.6*0.9995) #보유금의 60% 매수 (수수로 감안)
+                        print(a,"매수 평균단가:",upbit.get_avg_buy_price('KRW-OMG')) 
+                        a=a+1
 
-            if upbit.get_avg_buy_price('KRW-OMG') > 100 and current_price < upbit.get_avg_buy_price('KRW-OMG') * 0.86 : #-14% 손절
-                btc = get_balance("BTC")
-                if btc > 0.00008:
-                    upbit.sell_market_order("KRW-OMG", btc)
-                    print("손절 진행")
-                
-                
+        
+        if upbit.get_avg_buy_price('KRW-OMG')*1.05 < current_price :
+            btc = get_balance("OMG")
+            print(btc)
+            if btc > 0.00008:
+             upbit.sell_market_order("KRW-OMG", btc)
+             a=0
+             print("익절") 
+              
+            
+        if upbit.get_avg_buy_price('KRW-OMG')*0.95 > current_price : # -5% 1차 추매 
+            if krw > 5000:
+                if c<2: 
+                    upbit.buy_market_order("KRW-OMG", krw*0.4*0.9995) 
+                    print("1차 추가 매수")
+                    c=c+1
+                    
+        if upbit.get_avg_buy_price('KRW-OMG')*0.9 > current_price : #  -10% 2차 추매
+            if krw > 5000:
+                if b<2: 
+                    upbit.buy_market_order("KRW-OMG", krw*0.9995) 
+                    print("2차 추가 매수")
+                    b=b+1            
+
+        if upbit.get_avg_buy_price('KRW-OMG')*0.87 > current_price : # -13%되면 손절 
+            if btc > 0.00008:
+             upbit.sell_market_order("KRW-OMG", btc)
+             print("익절")              
+                 
         time.sleep(1)
     except Exception as e:
         
